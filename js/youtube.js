7af48d2cc6e81804dda6b3c6283ff04478a83fd0
Javier C <Javier@itsnotartitscode.com>
2012-05-14 12:57:42 -0700

    youtube playr

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
-
-	$('#player').youTubeEmbed("http://www.youtube.com/watch?v=Gui48cja7g0");
+	alert(jQuery.browser.mobile) 
+	if (!jQuery.browser.mobile) {
+		$('.mobile').remove();
+		$('#player').youTubeEmbed("http://www.youtube.com/watch?v=Gui48cja7g0");
+	}
+	 if(jQuery.browser.mobile) {	
+		$('.flashContainer').remove();
+	}
 });
