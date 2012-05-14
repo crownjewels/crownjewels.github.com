55ea1c81b0ade1dacb36876bbc369550a93f7340
Javier C <Javier@itsnotartitscode.com>
2012-05-14 11:37:31 -0700

    youtube vid change

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
 
-	$('#player').youTubeEmbed("http://www.youtube.com/watch?v=FXCC40HQIg4");
+	$('#player').youTubeEmbed("http://www.youtube.com/watch?v=Gui48cja7g0");
 });
