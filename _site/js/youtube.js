0bf3152303a9fb168274361e37dc3e4941c6c34b
Javier C <Javier@itsnotartitscode.com>
2012-05-16 22:58:00 -0600

    SEO optimizations

--- _site/js/youtube.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){ 
-	if (!jQuery.browser.mobile) {
-		$('.mobile').remove();
-		$('#player').youTubeEmbed("http://www.youtube.com/watch?v=Gui48cja7g0");
-	}
-	 if(jQuery.browser.mobile) {	
-		$('.flashContainer').remove();
-	}
-});
