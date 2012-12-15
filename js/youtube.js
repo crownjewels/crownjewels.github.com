7241fe6ace05a2b7fdfb35032436b7e4d30e42ca
Javier C <youracow@gmail.com>
2012-12-15 04:42:49 -0700

    check

--- js/youtube.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){ 
-	if (!jQuery.browser.mobile) {
-		$('.mobile').remove();
-		$('#player').youTubeEmbed("http://www.youtube.com/watch?v=NomMKITKUI4");
-	}
-	 if(jQuery.browser.mobile) {	
-		$('.flashContainer').remove();
-	}
-});
